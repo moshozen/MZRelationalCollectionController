b7faf34bbf186bd508c78d4fd52c3a869ce9725e
Mat Trudel <mat@geeky.net>
2015-11-26 14:49:31 -0500

    Improved duration picking in track edit

--- Example/MZRelationalCollectionController/Controllers/MZTrackEditViewController.m
+++ Example/MZRelationalCollectionController/Controllers/MZTrackEditViewController.m
@@ -10,10 +10,10 @@
 
 #import "Song.h"
 
-@interface MZTrackEditViewController ()
+@interface MZTrackEditViewController () <UIPickerViewDelegate, UIPickerViewDataSource>
 @property IBOutlet UITextField *titleField;
 @property IBOutlet UITextField *durationField;
-@property IBOutlet UIDatePicker *durationPicker;
+@property IBOutlet UIPickerView *durationPicker;
 @property IBOutlet UITableViewCell *titleCell;
 @property IBOutlet UITableViewCell *durationCell;
 @end
@@ -34,27 +34,11 @@
     [self updateUIFromAlbum];
 }
 
-- (IBAction)editingDidBegin:(UITextField *)textField
-{
-    if (textField == self.durationField && self.track.duration == 0) {
-        self.track.duration = self.durationPicker.countDownDuration;
-        [self updateUIFromAlbum];
-    }
-}
-
 - (IBAction)textDidChange:(UITextField *)textField
 {
     if (textField == self.titleField) {
         self.track.title = textField.text;
     }
-}
-
-- (IBAction)dateDidChange:(UIDatePicker *)datePicker
-{
-    if (datePicker == self.durationPicker) {
-        self.track.duration = datePicker.countDownDuration;
-    }
-    [self updateUIFromAlbum];
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath
@@ -70,7 +54,47 @@
 
 - (void)updateUIFromAlbum
 {
-    self.durationField.text = [NSString stringWithFormat:@"%ld seconds", (NSInteger)self.track.duration];
+    NSInteger minutes = fmod(trunc(self.track.duration / 60.0), 60.0);
+    NSInteger seconds = fmod(self.track.duration, 60.0);
+    [self.durationPicker selectRow:minutes inComponent:0 animated:NO];
+    [self.durationPicker selectRow:seconds inComponent:1 animated:NO];
+    self.durationField.text = [NSString stringWithFormat:@"%02ld:%02ld", minutes, seconds];
+}
+
+- (NSTimeInterval)durationFromPicker
+{
+    NSInteger minutes = [self.durationPicker selectedRowInComponent:0];
+    NSInteger seconds = [self.durationPicker selectedRowInComponent:1];
+    return (minutes * 60) + seconds;
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    if (component == 0) {
+        return 30;
+    } else {
+        return 60;
+    }
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if (component == 0) {
+        return [NSString stringWithFormat:@"%ld min", row];
+    } else {
+        return [NSString stringWithFormat:@"%ld sec", row];
+    }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    self.track.duration = [self durationFromPicker];
+    [self updateUIFromAlbum];
 }
 
 @end
